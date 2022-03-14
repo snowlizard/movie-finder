26db4379796ad52d362cb3dafecc9390a7d1114d
julian <illegal_burrito@live.com>
2022-03-14 11:31:31 -0700

    refactored async function

--- server/app.js
+++ server/app.js
@@ -11,28 +11,16 @@ app.use(morgan('dev'));
 // make sure to append the '&apikey=8730e0e' parameter
 
 app.get('/', (req, res) => {
-    let key = req.query.t ?
-            req.query.t
-            : req.query.i;
-
-    const getMovie = async (key)  => {
-        let a;
-        if(req.query.i){
-            a = await axios.get(`https://www.omdbapi.com/?i=${req.query.i}&apikey=e1d54a0`);
-            key = req.query.i
-        }else{
-            a = await axios.get(`https://www.omdbapi.com/?t=${req.query.t}&apikey=e1d54a0`);
-            key = req.query.t;
-        }
-        movies[key] = a.data;
-
-        res.status(200);
-        res.json(movies[key]);
-    }
-
-    if(key === undefined) res.send('Movie Finder');
+    if(Object.keys(req.query).length <= 0) res.send('Movie Finder');
     else{
-        getMovie(key);
+        let key = Object.keys(req.query)[0];
+        let data = req.query[key];
+
+        ( async () => {
+            let movie = await axios.get(`https://omdbapi.com/?${key}=${data}&apikey=e1d54a0`);
+            movies[data] = movie.data;
+            res.send(movies[data]);
+        })();
     }
 })
 
@@ -41,28 +29,16 @@ app.get('/', (req, res) => {
 module.exports = app
 
 /*
-    let key = req.query.t ?
-            req.query.t
-            : req.query.i;
-
-    const getMovie = async (key)  => {
-        let a;
-        if(req.query.i){
-            a = await axios.get(`https://www.omdbapi.com/?i=${req.query.i}&apikey=e1d54a0`);
-            key = req.query.i
-        }else{
-            a = await axios.get(`https://www.omdbapi.com/?t=${req.query.t}&apikey=e1d54a0`);
-            key = req.query.t;
-        }
-        movies[key] = a.data;
-
-        res.status(200);
-        res.json(movies[key]);
-    }
-
-    if(key === undefined) res.send('Movie Finder');
+    if(Object.keys(req.query).length <= 0) res.send('Movie Finder');
     else{
-        getMovie(key);
+        let key = Object.keys(req.query)[0];
+        let data = req.query[key];
+
+        ( async () => {
+            let movie = await axios.get(`https://omdbapi.com/?${key}=${data}&apikey=e1d54a0`);
+            movies[data] = movie.data;
+            res.send(movies[data]);
+        })();
     }
 */
 
